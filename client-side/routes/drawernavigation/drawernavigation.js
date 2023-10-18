80ffa1c23d4f6e9b2a4f82e55f83175800878631
Jerry-Khobby <jerrymardeburg@gmail.com>
2023-10-17 21:43:03 -1200

    after updating all the folders and making changes

---
+++ client-side/routes/drawernavigation/drawernavigation.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const drawernavigation = () => {
+  return (
+    <View>
+      <Text>drawernavigation</Text>
+    </View>
+  )
+}
+
+export default drawernavigation
+
+const styles = StyleSheet.create({})
