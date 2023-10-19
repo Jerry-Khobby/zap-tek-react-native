63ce515c6c4ac13f0e0dd8ad678b875d2a7fbdc3
Jerry-Khobby <jerrymardeburg@gmail.com>
2023-10-19 02:35:04 -1200

    finishing up the signup screen

--- client-side/screens/registration/screen_3_signup.jsx
+++ client-side/screens/registration/screen_3_signup.jsx
@@ -1,17 +1,27 @@
-import {Text, View,TouchableOpacity,TextInput} from 'react-native'
+import {Text, View,TouchableOpacity,TextInput,ScrollView} from 'react-native'
 import React,{useState} from 'react'
 import twrnc from 'tailwind-react-native-classnames';
 import Icon_Ant from "react-native-vector-icons/AntDesign"
-import Icon_EvilIcons from "react-native-vector-icons/EvilIcons";
 import Icon_Fontiso from "react-native-vector-icons/FontAwesome";
 
 const SignUp = () => {
     const [isToggled,setIsToggled]=useState(false);
-
-
     const handleToggle=()=>{
         setIsToggled(!isToggled);
     }
+
+    const [inputs,setInputs]=useState({});
+
+    
+    const handleChange = (name, value) => {
+        setInputs(values => ({ ...values, [name]: value }));
+      };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(JSON.stringify(inputs,null,2));
+  }
+
 
   return (
     <View style={twrnc`flex w-full h-full flex-col mt-11 relative`}>
@@ -28,10 +38,10 @@ const SignUp = () => {
 </View>
 
 {/** I change the button and make it a text input field where the first field will be username , the next one will password and the last field will email add  */}
-<View style={twrnc`mt-44 mx-5`}>
+<View style={twrnc`mt-32 mx-5`}>
     <View style={twrnc`mb-2 flex flex-col  justify-between`}>
     <Text style={twrnc`font-normal text-base text-gray-600`}>Username</Text>
-    <View style={twrnc`mb-3 -mt-3`}>
+    <View style={twrnc`mb-3 -mt-4`}>
     <TextInput
     style={twrnc`
     h-12 w-full pt-5
@@ -40,12 +50,15 @@ const SignUp = () => {
     underlineColorAndroid="transparent"
     borderBottomWidth={1} // For iOS
     borderBottomColor="gray" //
+    value={inputs.username} 
+    onChange={handleChange}
+
     />
     </View>
     </View>
     <View style={twrnc`mb-2 flex flex-col  justify-between`}>
     <Text style={twrnc`font-normal text-base text-gray-600`}>Password</Text>
-    <View style={twrnc`mb-3 -mt-3`}>
+    <View style={twrnc`mb-3 -mt-4`}>
     <TextInput
     style={twrnc`
     h-12 w-full pt-5
@@ -54,12 +67,15 @@ const SignUp = () => {
     underlineColorAndroid="transparent"
     borderBottomWidth={1} // For iOS
     borderBottomColor="gray" //
+    value={inputs.password} 
+    onChange={handleChange}
+
     />
     </View>
     </View>
     <View style={twrnc`mb-2 flex flex-col  justify-between`}>
     <Text style={twrnc`font-normal text-base text-gray-600`}>Email Address</Text>
-    <View style={twrnc`mb-3 -mt-3`}>
+    <View style={twrnc`mb-3 -mt-4`}>
     <TextInput
     style={twrnc`
     h-12 w-full pt-5
@@ -68,6 +84,9 @@ const SignUp = () => {
     underlineColorAndroid="transparent"
     borderBottomWidth={1} // For iOS
     borderBottomColor="gray" //
+    value={inputs.email} 
+    onChange={handleChange}
+
     />
     </View>
     </View>
@@ -92,7 +111,7 @@ const SignUp = () => {
 
 {/** this is the last button that will stack to the button of the screen and will  */}
 <View style={twrnc`flex items-center   absolute bottom-0 mt-auto bg-purple-500 w-full h-28 flex`}>
-        <TouchableOpacity style={twrnc`w-full flex items-center justify-center pt-5`}>
+        <TouchableOpacity style={twrnc`w-full flex items-center justify-center pt-5`} onPress={handleSubmit}>
           <Text style={twrnc`text-white text-center font-semibold`}>Sign Up </Text>
         </TouchableOpacity>
       </View>
