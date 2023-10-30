fe7cf660f29863353386a7a5cc01d48137fac0f4
Livingstone Ackah <kofiackah360@gmail.com>
2023-10-30 01:59:32 +0000

    All Merge

--- client-side/App.js
+++ client-side/App.js
@@ -1,31 +1,24 @@
-import React from 'react'
-import { StatusBar } from 'expo-status-bar';
-import {View } from 'react-native'
-import twrnc from 'tailwind-react-native-classnames';
-import MainStackRouter from './routes/mainstacknavigator/MainStackRouter';
-import { NavigationContainer } from '@react-navigation/native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import twrnc from "tailwind-react-native-classnames";
+import MainStackRouter from "./routes/mainstacknavigator/MainStackRouter";
+import { NavigationContainer } from "@react-navigation/native";
 /* import BottomTabNavigator from "./routes/tabnavigation/tabnavigation"; */
-import 'react-native-gesture-handler';
-import { DarkModeProvider } from './context/darkmode';
-
-
-
-
-
-
+import "react-native-gesture-handler";
+import { DarkModeProvider } from "./context/darkmode";
 
 const App = () => {
-
   return (
     <View style={twrnc`flex-1`}>
-     {/** all the components will here  */}
-        <MainStackRouter/>
-  {/*  <DrawerMainComponent /> */}
-    <StatusBar style='light-content'/>
-    </DarkModeProvider>
+      {/** all the components will here  */}
+      <DarkModeProvider>
+        <MainStackRouter />
+        {/*  <DrawerMainComponent /> */}
+        <StatusBar style="light-content" />
+      </DarkModeProvider>
     </View>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
