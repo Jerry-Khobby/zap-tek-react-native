c2ed066ebe23fe8620e7f2afbaebb85e3f36c062
Jerry-Khobby <jerrymardeburg@gmail.com>
2023-11-01 01:20:45 -1200

    done with the order confirmed screen

--- client-side/screens/homescreens/orderConfirm.jsx
+++ client-side/screens/homescreens/orderConfirm.jsx
@@ -8,6 +8,7 @@ import {
 } from "react-native";
 import React from "react";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { useDarkMode } from "../../context/darkmode";
 
 const OrderConfirmed = ({navigation}) => {
 
@@ -25,21 +26,33 @@ const backWardNavigation=()=>{
 const orderNavigation=()=>{
   navigation.navigate("screen18");
 }
+
+const {isDarkMode}=useDarkMode();
   return (
-    <View className="flex-1 bg-white">
+    <View className={`flex-1 ${isDarkMode ? 'bg-black' : 'bg-white'}`}>
       <TouchableOpacity
-        className="bg-gray-100 w-12 h-12 rounded-full mx-5 mt-11 items-center justify-center"
-        onPress={backWardNavigation}
-      >
-        <MaterialCommunityIcons name="arrow-left" size={24} />
-      </TouchableOpacity>
+  className={`w-12 h-12 mx-5 mt-11 items-center justify-center rounded-full ${
+    isDarkMode ? 'bg-gray-700' : 'bg-gray-100'
+  }`}
+  onPress={backWardNavigation}
+>
+  <MaterialCommunityIcons
+    name="arrow-left"
+    size={24}
+    color={isDarkMode ? 'white' : 'black'}
+  />
+</TouchableOpacity>
+
       <Image
         source={require("../../assets/Order.png")}
         className="relative -top-16 mx-auto my-auto"
       />
       <View className="relative -top-36">
-        <Text className="text-4xl font-bold text-center">Order Confirmed!</Text>
-        <Text className="mx-5 text-center text-lg text-gray-500">
+      <Text className={`text-4xl font-bold text-center ${isDarkMode ? 'text-white' : 'text-black'}`}>
+  Order Confirmed!
+</Text>
+
+<Text className={`mx-5 text-center text-lg ${isDarkMode ? 'text-gray-400' : 'text-gray-500'}`}>
           Your order has been confirmed, we will send you confirmation email
           shortly.
         </Text>
