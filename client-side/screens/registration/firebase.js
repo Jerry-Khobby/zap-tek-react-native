acce9adf179b0fca06478c069f065e0d7a71509f
Jerry-Khobby <jerrymardeburg@gmail.com>
2023-11-01 11:44:59 -1200

    after setting up the the firebase for signup

--- client-side/screens/registration/firebase.js
+++ client-side/screens/registration/firebase.js
@@ -1,20 +1,43 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID, FIREBASE_MEASUREMENT_ID } from '@env';
+import * as firebase from "firebase/compat";// I used compat because of the version of the firebase
+import 'firebase/auth';
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+} from '@env'
+
+
+
 
 
 
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId:FIREBASE_MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_ID,
-  measurementId:FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyAXCojgclX9reonVX3X3Thl7M4duPeEpZw",
+  authDomain: "zaptechmobileapp.firebaseapp.com",
+  projectId: "zaptechmobileapp",
+  storageBucket: "zaptechmobileapp.appspot.com",
+  messagingSenderId: "112260528090",
+  appId: "1:112260528090:web:cbab4201aa8ff76ea2f2e3",
+  measurementId: "G-DBVXVN15E8",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+let app;
+//a conditional statement to check whether there is no  firebase configuration, if there is none, we initialize it with the else statement.
+if (firebase.apps.length === 0){
+     app = firebase.initializeApp(firebaseConfig);
+}
+else{
+    app = firebase.app()
+}
+
+//store the authentication details in a conatiner auth and export.
+const auth = firebase.auth()
+
+
+export {auth};
