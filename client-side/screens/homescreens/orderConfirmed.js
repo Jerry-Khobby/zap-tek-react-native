b5f7ed73c805141b31be3d80a1b144e5b6f51f1e
Livingstone Ackah <kofiackah360@gmail.com>
2023-11-01 01:58:51 +0000

    Start register Darkmode

--- client-side/screens/homescreens/orderConfirmed.js
+++ client-side/screens/homescreens/orderConfirmed.js
@@ -9,21 +9,41 @@ import {
 import React from "react";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
+import { useDarkMode } from "../../context/darkmode";
+
 const orderConfirmed = () => {
+  const { isDarkMode } = useDarkMode();
+
   return (
-    <View className="flex-1 bg-white">
+    <View className={`flex-1 bg-${!isDarkMode ? "white" : "black"}`}>
       <TouchableOpacity
-        className="bg-gray-100 w-12 h-12 rounded-full mx-5 mt-5 items-center justify-center"
+        className={`bg-gray-${
+          !isDarkMode ? "200" : "700"
+        } w-12 h-12 rounded-full mx-5 mt-5 items-center justify-center`}
         onPress={() => alert("Navigate Back is under construction")}
       >
-        <MaterialCommunityIcons name="arrow-left" size={30} />
+        <MaterialCommunityIcons
+          name="arrow-left"
+          size={30}
+          color={`${!isDarkMode ? "black" : "white"}`}
+        />
       </TouchableOpacity>
       <Image
-        source={require("../../assets/Order.png")}
+        source={
+          !isDarkMode
+            ? require("../../assets/phonelight.png")
+            : require("../../assets/phonedark.png")
+        }
         className="relative -top-16 mx-auto my-auto"
       />
       <View className="relative -top-36">
-        <Text className="text-4xl font-bold text-center">Order Confirmed!</Text>
+        <Text
+          className={`text-${
+            !isDarkMode ? "black" : "white"
+          } text-4xl font-bold text-center`}
+        >
+          Order Confirmed
+        </Text>
         <Text className="mx-5 text-center text-lg text-gray-500">
           Your order has been confirmed, we will send you confirmation email
           shortly.
@@ -31,7 +51,9 @@ const orderConfirmed = () => {
       </View>
       <Text
         onPress={() => alert("Currently not working")}
-        className="text-2xl text-center mx-5 mb-10 bg-gray-200 p-3 rounded-xl text-gray-400 font-semibold"
+        className={`text-2xl text-center mx-5 mb-10 bg-gray-${
+          !isDarkMode ? "200" : "700"
+        } p-3 rounded-xl text-gray-400`}
       >
         Go to Orders
       </Text>
